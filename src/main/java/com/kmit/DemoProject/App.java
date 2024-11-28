59602e840c4e511ff4ce13f24370a6254b9abc34
veerla.supritha@gmail.com <vishal.veerla@gmail.com>
2024-11-28 06:51:11 +0530

    Additon of MavenJava project

---
+++ src/main/java/com/kmit/DemoProject/App.java
@@ -0,0 +1,10 @@
+package com.kmit.DemoProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Project updated");
+    }
+}
